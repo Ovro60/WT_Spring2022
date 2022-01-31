413690034c131add7a6bab4c6c1f30d788d8173b
Ovro60 <Ovro60@gmail.com>
2022-01-31 17:40:49 +0600

    Task 1

---
+++ Task 1/Test1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registration Form</h2>
+
+<form action="/action_page.php">
+  First Name:
+  <input type="text" name="name" placeholder="enter your first name">
+  <br>
+  Last Name:
+  <input type="text" name="name" placeholder="enter your last name">
+  <br>
+  Age:      
+  <input type="text" name="name" placeholder="enter your age">
+  <br>
+  
+  Designation:
+  <input type="radio"name= ""> Junior Programmer 
+  <input type="radio"name=""> Senior programmer
+  <input type="radio"name=""> Project Lead
+  <br>
+  
+  Preferred language
+  <input type="checkbox" id="" name="vehicle1" value="Bike">
+<label for="vehicle1"> JAVA</label>
+<input type="checkbox" id="" name="vehicle2" value="Car">
+<label for="vehicle2"> PHP</label>
+<input type="checkbox" id="" name="vehicle3" value="Boat">
+<label for="vehicle3"> C++</label><br>
+  
+  E-mail:
+  <input type="text" name="name" placeholder="enter your email">
+  <br>
+  Password:
+  <input type-"password" name="password" placeholder="enter your password">
+  <br>
+  
+  <form action="/action_page.php">
+  <label for="myfile">Please choose a file:</label>
+  <input type="file" id="myfile" name="myfile"><br><br>
+
+  
+  <button type="button" onclick="alert('')">Submit</button>
+  <button type="button" onclick="alert('')">Reset</button>
+</form>
+</body>
+</html>
+
